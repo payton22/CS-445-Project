85948b440982b52e48ac7037bd2ac53e274e4bee
Payton <pjknoch@sbcglobal.net>
2021-04-07 18:10:14 -0700

    Added parser for the gateway router

--- sniffer.py
+++ sniffer.py
@@ -10,7 +10,7 @@ packets = []
 
 def packet_sniffer():
     print('Calling packet sniffer function.')
-    pkt = sniff(filter='tcp', count=5, iface='eth1', prn=packet_storage)
+    pkt = sniff(filter='tcp and src=', count=5, iface='eth1', prn=packet_storage)
 
 def packet_storage(packet):
     packets.append(packet)
@@ -32,7 +32,54 @@ def send_back():
         print('new_packet:', new_packet.show())
 
         r1 = sr1(new_packet, timeout=2)
+
+def parse_payload(data):
+
+    # Look for this at the end of the payload
+    test_field = "|DUP_PL_DEST:"
+                                                         
+    if test_field in data:
+        # end info = information after the main payload 
+        end_info = data.partition(test_field)[2] 
+                                                         
+    # Split the end substring by | delimiter 
+    end_list = end_info.split('|')
+                                                         
+    return end_list
+
+# Append the IP address info of the original sender (before being 
+# forwarded by the router)
+# Format is: |ORIG_SOURCE:xxxx.xxxx.xxxx.xxxx
+def append_orig_source(data, orig_source):
+    new_data = data + '|ORIG_SOURCE:' + orig_source
+
+    return new_data
+
+
+def forward_packets():
+    for packet in packets:
+        data = packet[Raw].load
+        data = append_orig_source(data, orig_source)
+
+        forwarded_packet = IP()/TCP()/Raw(load=data)
+
+        info_list = parse_payload(data)
         
+        # IP address is always the first in the list 
+        ip_addr = info_list[0]
+        
+
+        # Source = hardcoded router's IP address 
+        forwarded_packet[IP].src = "192.168.56.105"
+        forwarded_packet[IP].dst = ip_addr
+        
+        forwarded_packet[TCP].sport = packet[TCP].sport
+        forwarded_packet[IP].dport = packet[TCP].dport
+
+        print('Forwarded packet:', forwrded_packet.show())
+
+        r1 = sr1(forwarded_packet, timeout=5)
+    
 
 def print_packets():
     for packet in packets:
